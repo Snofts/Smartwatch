28360b32c767d2b4463d7da4abf3d69eeeedfb38
Snofts <samuelsnofts@gmail.com>
2025-04-26 13:00:20 +0100

    New commit

--- Script.js
+++ Script.js
@@ -1,165 +1,227 @@
-'use strict';
+"use strict";
 
-const header = document.querySelector('.header');
-const headerBtn = document.querySelector('.header__btn');
-const bannerBtn = document.querySelector('.banner__btn');
-const circle1 = document.querySelector('.circle-1');
-const circle2 = document.querySelector('.circle-2');
-const circle3 = document.querySelector('.circle-3');
-const circle4 = document.querySelector('.circle-4');
-const collectionBtn = document.querySelector('.collection__display--btn');
-const memberIcon1 = document.querySelector('.member__icon--1');
-const memberIcon2 = document.querySelector('.member__icon--2');
-const memberIcon3 = document.querySelector('.member__icon--3');
-const memberIcon4 = document.querySelector('.member__icon--4');
-const memberIconCross1 = document.querySelector('.member__icon--cross1');
-const memberIconCross2 = document.querySelector('.member__icon--cross2');
-const memberIconCross3 = document.querySelector('.member__icon--cross3');
-const memberIconCross4 = document.querySelector('.member__icon--cross4');
-const mapContactBtn1 = document.querySelector('.map__contact--side1--button');
-const mapContactBtn2 = document.querySelector('.map__contact--side2--button');
-const mapCross = document.querySelector('.map__contact--cross');
-const video = document.querySelector('.video');
-const videoCross = document.querySelector('.video__cross');
-const popup = document.querySelector('.popup');
-const popupCross = document.querySelector('.popup__cross');
-const popupOverlay = document.querySelector('.popup__overlay');
+const header = document.querySelector(".header");
+const trigger = document.querySelector(".trigger");
+const headerBtn = document.querySelector(".header__btn");
+const banner = document.querySelector(".banner");
+const bannerBtn = document.querySelector(".banner__btn");
+const circle1 = document.querySelector(".circle-1");
+const circle2 = document.querySelector(".circle-2");
+const circle3 = document.querySelector(".circle-3");
+const circle4 = document.querySelector(".circle-4");
+const collectionBtn = document.querySelector(".collection__display--btn");
+const memberIcon1 = document.querySelector(".icon__1--open");
+const memberIcon2 = document.querySelector(".icon__2--open");
+const memberIcon3 = document.querySelector(".icon__3--open");
+const memberIcon4 = document.querySelector(".icon__4--open");
+const memberIconShow1 = document.querySelector(".icon__1--close");
+const memberIconShow2 = document.querySelector(".icon__2--close");
+const memberIconShow3 = document.querySelector(".icon__3--close");
+const memberIconShow4 = document.querySelector(".icon__4--close");
+const memberIconCross1 = document.querySelector(".member__icon-cross1");
+const memberIconCross2 = document.querySelector(".member__icon-cross2");
+const memberIconCross3 = document.querySelector(".member__icon-cross3");
+const memberIconCross4 = document.querySelector(".member__icon-cross4");
+const mapContactSide1 = document.querySelector(".map__contact--side1");
+const mapContactSide2 = document.querySelector(".map__contact--side2");
+const mapContactBtn1 = document.querySelector(".map__contact--side1--button");
+const mapContactBtn2 = document.querySelector(".map__contact--side2--button");
+const mapCross = document.querySelector(".map__contact--crosssvg");
+const video = document.querySelector(".video");
+const videoCross = document.querySelector(".video__cross");
 
-console.log(popupOverlay);
+const videoClose = document.querySelectorAll(
+  ".video > *:not(.video__content > *), .video__cross"
+);
+const teamIcon = document.querySelectorAll(".team__member--icon")
+
+const popup = document.querySelector(".popup");
+const popupCross = document.querySelector(".popup__cross");
+const popupOverlay = document.querySelector(".popup__overlay");
+const popupBtn = document.querySelector(".checkout__btn");
+
+// Functions
+
+// Popup Open and close function
+const openPopup = function (section) {
+  section.classList.remove("hidden");
+  section.classList.remove("animate__zoomout");
+  section.classList.add("animate__zoomin");
+};
+
+const closePopup = function (section) {
+  section.classList.remove("animate__zoomin");
+  section.classList.add("animate__zoomout");
+  setTimeout(function () {
+    section.classList.add("hidden");
+  }, 300);
+};
+
+// Team member Icon Open and Close Function
+// open
+const memberIconOpen = function (class1, class2) {
+    class1.classList.add("hidden");
+    class2.classList.remove("hidden");
+    class2.classList.remove("animate__conceal");
+    class2.classList.add("animate__reveal");
+  };
+
+// Close
+  const memberIconClose = function (class1, class2) {
+    setTimeout(function () {
+      class2.classList.add("hidden");
+    }, 700);
+    class1.classList.remove("hidden");
+    class2.classList.remove("animate__reveal");
+    class2.classList.add("animate__conceal");
+  };
+
+//   Card flip animation
+// Flip
+const cardFlip = function(side1, side2){
+    side1.style.transform = 'rotateY(180deg)';
+    side2.style.transform = 'rotateY(0deg)';
+}
+
+// Reverse flip
+const cardReverseFlip = function(side1, side2){
+    side1.style.transform = 'rotateY(0deg)';
+    side2.style.transform = 'rotateY(-180deg)';
+}
 
 
 ////////////////////////////////////////////////////////////////////////////
+////////////////////////////
 
 // Sticky Nav
+const headerHeight = header.getBoundingClientRect().height;
 
+const stickyNav = function(entries){
+  const [entry] = entries;
+  console.log(entry);
+//  entries.forEach(entry => {
+  
+//  })
+  if(!entry.isIntersecting) header.classList.add("sticky")
+    else header.classList.remove("sticky")
+}
+
+const bannerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`
+});
+
+bannerObserver.observe(banner);
 
 // Open watch popup
 
-headerBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    popup.classList.remove('hidden');
-    popup.classList.add('animate__zoomin');
-    console.log(`It works`)
+headerBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  openPopup(popup);
 });
 
 // Close watch popup
-popupCross.addEventListener('click', function(e){
-    e.preventDefault();
-    popup.classList.add('hidden');
-});
+// popupCross.addEventListener('click', function(e){
+//     e.preventDefault();
+//     popup.classList.add('hidden');
+// });
 
-popupOverlay.addEventListener('click', function(e){
-    e.preventDefault();
-    popup.classList.add('hidden');
-    console.log('Close popup')
+popupOverlay.addEventListener("click", function (e) {
+  closePopup(popup);
 });
-
-popupOverlay.addEventListener('click', () => {
-    alert('Overlay clicked');
-});
-
 
 // Open Video popup
-bannerBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    video.classList.remove('hidden');
-    console.log("Here is the video")
-})
+bannerBtn.addEventListener("click", function (e) {
+  openPopup(video);
+});
 
 // Close Video popup
-videoCross.addEventListener('click', function(e){
-    e.preventDefault();
-    const openPopup = function(){
-        video.classList.add('hidden');
-    }
-    openPopup()
-})
+videoClose.forEach((element) => {
+  element.addEventListener("click", function () {
+    closePopup(video);
+    console.log("Element clicked!");
+  });
+});
 
-// Member 1 circle Open
-circle1.addEventListener('click', function(e){
-    e.preventDefault();
-    const openSocials = function(){
-        circle1.classList.add('hidden');
-        memberIcon1.classList.remove('hidden');
-        //add animation
-    }
-})
-
-// Member 1 circle close 
-// ****Chekc to see if it is ok not to put the codes inside the event handler into another function again then call it
-memberIconCross1.addEventListener('click', function(e){
-    e.preventDefault();
-    //reverse animation
-    memberIcon1.classList.add('hidden');
-    circle1.classList.remove('hidden');
-})
+// videoCross.addEventListener('click', function(e){
+//     e.preventDefault();
+//     // const openPopup = function(){
+//     //     video.classList.add('hidden');
+//     // }
+//     // openPopup()
+//     closePopup(video);
+// })
 
 
-// Member 2 circle Open
-circle2.addEventListener('click', function(e){
-    e.preventDefault();
-    const openSocials = function(){
-        circle2.classList.add('hidden');
-        memberIcon2.classList.remove('hidden');
-        //add animation
-    }
+teamIcon.forEach((el, i)=>{
+  el.addEventListener('click', function(e){
+    const loopPos = i + 1;
+    console.log(`memberIcon${loopPos}`);
+    const class1 = `memberIcon${loopPos}`;
+    const class2 = `memberIconShow${loopPos}`;
+    memberIconOpen(class1, class2)
+    // console.log(`memberIcon${e.target.dataset.id}`);
+    // const class1 = Number(`memberIcon${e.target.classList.value}`);
+    // const class2 = `memberIconShow${e.target.classList.value}`;
+    // console.log(class1, class2)
+    // memberIconOpen(class1, class2);
+  })
 })
 
-// Member 2 circle close 
-memberIconCross2.addEventListener('click', function(e){
-    e.preventDefault();
-    //reverse animation
-    memberIcon2.classList.add('hidden');
-    circle2.classList.remove('hidden');
-})
 
-// Member 3 circle Open
-circle3.addEventListener('click', function(e){
-    e.preventDefault();
-    const openSocials = function(){
-        circle3.classList.add('hidden');
-        memberIcon3.classList.remove('hidden');
-        //add animation
-    }
-})
+// // Team member1 circle Open
+// memberIcon1.addEventListener("click", function (e) {
+//   memberIconOpen(memberIcon1, memberIconShow1);
+// });
 
-// Member 3 circle close 
-memberIconCross3.addEventListener('click', function(e){
-    e.preventDefault();
-    //reverse animation
-    memberIcon3.classList.add('hidden');
-    circle3.classList.remove('hidden');
-})
+// // Team member1 circle close
+// memberIconCross1.addEventListener("click", function (e) {
+//   memberIconClose(memberIcon1, memberIconShow1);
+// });
 
-// Member 4 circle Open
-circle4.addEventListener('click', function(e){
-    e.preventDefault();
-    const openSocials = function(){
-        circle4.classList.add('hidden');
-        memberIcon4.classList.remove('hidden');
-        //add animation
-    }
-})
+// // Team member2 circle Open
+// memberIcon2.addEventListener("click", function (e) {
+//   memberIconOpen(memberIcon2, memberIconShow2);
+//   // console.log(`${e.target}`);
+//   // console.log(e.target)
+// });
 
-// Member 4 circle close 
-memberIconCross4.addEventListener('click', function(e){
-    e.preventDefault();
-    //reverse animation
-    memberIcon4.classList.add('hidden');
-    circle3.classList.remove('hidden');
-})
+// // Team member2 circle close
+// memberIconCross2.addEventListener("click", function (e) {
+//   memberIconClose(memberIcon2, memberIconShow2);
+//   console.log("Close function triggered");
+// });
+
+// // Team member3 circle Open
+// memberIcon3.addEventListener("click", function (e) {
+//   memberIconOpen(memberIcon3, memberIconShow3);
+// });
+
+// // Team member3 circle close
+// memberIconCross3.addEventListener("click", function (e) {
+//   memberIconClose(memberIcon3, memberIconShow3);
+// });
+
+// // Team member4 circle Open
+// memberIcon4.addEventListener("click", function (e) {
+//   memberIconOpen(memberIcon4, memberIconShow4);
+// });
+
+// // Team member4 circle close
+// memberIconCross4.addEventListener("click", function (e) {
+//   memberIconClose(memberIcon4, memberIconShow4);
+// });
 
 // Map Animation
-mapContactBtn1.addEventListener('click', function(e){
+mapContactBtn1.addEventListener("click", function (e) {
+    cardFlip(mapContactSide1, mapContactSide2)
+});
 
-})
+mapContactBtn2.addEventListener("click", function (e) {
+    cardReverseFlip(mapContactSide1, mapContactSide2)
+});
 
-mapContactBtn2.addEventListener('click', function(e){
-    // reverse animation
-    // collect form input
-})
-
-mapCross.addEventListener('click', function(e){
-    // reverse animation
-})
-
+mapCross.addEventListener("click", function (e) {
+    cardReverseFlip(mapContactSide1, mapContactSide2)
+});
